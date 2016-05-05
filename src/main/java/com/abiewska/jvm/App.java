df60449911ee337600c8c914519a16b0dc2341ae
abie115 <donicczka@gmail.com>
2016-05-05 14:47:52 +0200

    fix

--- src/main/java/com/abiewska/jvm/App.java
+++ src/main/java/com/abiewska/jvm/App.java
@@ -2,7 +2,7 @@ package com.abiewska.jvm;
 
 public class App {
 	public static void main(String[] args) {
-		//Benchmark benchmark = new Benchmark(1);
+		//Benchmark benchmark = new Benchmark(2);
 		Benchmark benchmark = new Benchmark(1);
 	}
 }
